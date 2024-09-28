5ed18dc5195d0adf9b9a8ba838f6864cca24dd2f
Spyei <caio.salamao1234@gmail.com>
2024-09-28 16:38:02 -0300

    feat: implementing mongoconnect function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Server } from "hyper-express";
 import { config } from "dotenv";
 import loadRoutes from "./utils/loadRoutes";
+import mongoConnect from "./utils/mongoConnect";
 config();
 
 const { PORT = 8080 } = process.env;
 const server = new Server();
 
 await loadRoutes(server);
+await mongoConnect();
 
 server.listen(<number>PORT, () => {
 	console.log(`Running on port ${PORT}`);
