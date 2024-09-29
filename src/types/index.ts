ab0cd13f366330f91087e09ef3ab5f0101205a88
Spyei <caio.salamao1234@gmail.com>
2024-09-29 16:25:10 -0300

    feat: new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,8 @@ export type RequestMethod = "get" | "post" | "put" | "delete" | "patch";
 export interface BaseRoute {
 	route: string;
 	method?: RequestMethod;
+	protected?: boolean;
+	onlyDevs?: boolean;
 	run: (req: Request, res: Response) => void;
 }
 
@@ -22,19 +24,40 @@ export enum StatusCodes {
 }
 
 export enum ErrorsMessages {
-	MISSING_CODE = "Missing auth code parameter",
-	INVALID_CODE = "Invalid auth code",
-	INTERNAL_SERVER_ERROR = "Internal server error"
+	"Internal server error" = 1,
+	"Missing auth code parameter" = 1000,
+	"Invalid auth code" = 1001,
+	"Missing auth token" = 1002,
+	"Invalid auth token" = 1003
 }
 
 export enum ErrorsCodes {
 	INTERNAL_SERVER_ERROR = 1,
 	MISSING_CODE = 1000,
-	INVALID_CODE = 1001
+	INVALID_CODE = 1001,
+	MISSING_TOKEN = 1002,
+	INVALID_TOKEN = 1003
 }
 
 export interface UserStructure {
 	_id: string;
 	username: string;
 	avatar: string;
+}
+
+export interface LinkStructure {
+	subdomain: string;
+	author: {
+		icon?: string;
+		name: string;
+		description?: string;
+	};
+	links: {
+		url: string;
+		label: string;
+		group?: string;
+		icon?: string;
+	};
+	groups?: string[];
+	background?: string;
 }
