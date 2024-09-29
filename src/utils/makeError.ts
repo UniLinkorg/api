0a58719f18cef68b8bb39ad7ed9d51de00937c4e
Spyei <caio.salamao1234@gmail.com>
2024-09-29 16:12:27 -0300

    chore: make error function

--- src/utils/makeError.ts
+++ src/utils/makeError.ts
@@ -1,4 +1,9 @@
-const makeError = (code: number, message: string) => {
+import { ErrorsCodes, ErrorsMessages } from "../types";
+
+const makeError = (code: number) => {
+	const mappedCode = Object.values(ErrorsCodes).find((value) => value === code);
+	const message = ErrorsMessages[<number>mappedCode ?? 1];
+
 	return {
 		code,
 		message
